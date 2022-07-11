88cf9aee7bad6c629cfa9e8554b5abca3548bdc8
Miguel <veruiz.miguel@gmail.com>
2022-07-10 23:34:03 -0300

    fix another errors

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -10,7 +10,7 @@ import { Auth } from '../models/auth.model';
 import { AuthService } from './auth.service';
 import { TokenService } from './token.service';
 
-fdescribe('AuthService', () => {
+describe('AuthService', () => {
   let authService: AuthService;
   let httpController: HttpTestingController;
   let tokenService: TokenService;
